45fbf6896635c76662af5e8fc9a3a613716e932f
dangtiensi <dangtiensi.vn@gmail.com>
2022-12-21 13:45:10 +0700

    commit

---
+++ script.lua
@@ -0,0 +1,5 @@
+-- https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dangtiensi/roblox/main/ui.lua"))()
+local Window = Library.CreateLib("Menu Cheat - siben.vn", "Ocean")
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
