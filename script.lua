488103e2cc47668ee85c552de1652208282c4ad8
dangtiensi <dangtiensi.vn@gmail.com>
2022-12-21 16:16:05 +0700

    commit

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 -- https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dangtiensi/roblox/main/ui.lua"))()
-local NoClip = loadstring(game:HttpGet("https://raw.githubusercontent.com/dangtiensi/roblox/main/noclip.lua"))()
+local Library = loadstring(game:HttpGet(("https://raw.githubusercontent.com/dangtiensi/roblox/main/ui.lua"),true))()
+local NoClip = loadstring(game:HttpGet(("https://raw.githubusercontent.com/dangtiensi/roblox/main/noclip.lua"),true))()
 local Window = Library.CreateLib("Menu Cheat - siben.vn", "Ocean")
 local Main = Window:NewTab("Main")
 local MainSection = Main:NewSection("Main")
