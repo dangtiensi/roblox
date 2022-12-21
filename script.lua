dc634f5d9585401fc3787c99696191065e4cdd93
dangtiensi <dangtiensi.vn@gmail.com>
2022-12-21 14:56:11 +0700

    commit

--- script.lua
+++ script.lua
@@ -6,20 +6,21 @@ local MainSection = Main:NewSection("Main")
 
 -- Button Toggle
 local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
+local Button = Instance.new("ImageLabel")
 ScreenGui.Parent = game.CoreGui
 ScreenGui.Name = "ButtonToggle"
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 ScreenGui.ResetOnSpawn = false
 
-Frame.Name = "Button"
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(74, 99, 135)
-Frame.ClipsDescendants = true
-Frame.AnchorPoint = Vector2.new(0.5, 0.5)
-Frame.Position = UDim2.new(0.5, 0, 0.75, 0)
-Frame.Size = UDim2.new(0, 36, 0, 36)
-Frame.InputBegan:Connect(function(InputObject)
+Button.Name = "Button"
+Button.Parent = ScreenGui
+Button.BackgroundColor3 = Color3.fromRGB(74, 99, 135)
+Button.ClipsDescendants = true
+Button.AnchorPoint = Vector2.new(0.5, 0.5)
+Button.Position = UDim2.new(0.027, 0, 0.063, 0)
+Button.Image = "rbxassetid://11877618750"
+Button.Size = UDim2.new(0, 36, 0, 36)
+Button.InputBegan:Connect(function(InputObject)
     if InputObject.UserInputType == Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch then
         Library:ToggleUI()
     end
